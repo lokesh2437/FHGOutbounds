b8439b2fec2dd445876a4fdb598ee1676e6fad23
lokesh2437 <lokeshd@retailsols.com>
2015-09-04 23:35:21 +0300

    Initial Commit

---
+++ FHG_CC_S2/src/com/retailsols/db/DBUtility.java
@@ -0,0 +1,49 @@
+package com.retailsols.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
+public class DBUtility {
+
+	private static Connection conn = null;
+
+	private DBUtility(Properties properties, Logger logger) {
+		try {
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			conn = DriverManager.getConnection(
+					"jdbc:oracle:thin:@" + properties.getProperty("host") + ":"
+							+ properties.getProperty("port") + ":"
+							+ properties.getProperty("sid"),
+					properties.getProperty("user"),
+					properties.getProperty("pass"));
+
+		} catch (Exception e) {
+			logger.error("", e);
+		}
+	}
+
+	public static Connection getConnection(Properties properties, Logger logger) {
+		if (conn == null) {
+			new DBUtility(properties, logger);
+			return conn;
+		} else {
+			return conn;
+		}
+	}
+
+	public static boolean closeConnection(Connection con, Logger logger) {
+		boolean b = false;
+		try {
+			if (con != null) {
+				con.close();
+			}
+		} catch (Exception e) {
+			logger.error("", e);
+		}
+		return b;
+
+	}
+}
---
+++ FHG_CC_S2/src/com/retailsols/fhg/main/ProcessPendInvMain.java
@@ -0,0 +1,8 @@
+package com.retailsols.fhg.main;
+
+public class ProcessPendInvMain {
+
+	public static void main(String[] args) {
+
+	}
+}
